68e2f7b7b78a1e4ee69ecd9d0636981c753eef5a
Jon Poret <poret.jon@gmail.com>
2023-01-03 18:47:50 -0500

    Obtaining beatmap Title and Artist works.

--- TopPlayGrabber.py
+++ TopPlayGrabber.py
@@ -10,18 +10,20 @@ def pullTopPlays(userid):
    scores = []
    for i in range(25) :
       #grab the top 25 plays
-      #I dont see the point in grabbing the top 100 plays
-      #given that most songs in osu are on spotify the top 25
-      #should be enough to get a solid playlist made
-      mapID = api.get_user_best(userid)[i].beatmap_id
-      mapName = api.beatmap(mapID).title
-      mapArtist = api.beatmap(mapID).artist
-      print("Title: ", mapName, ", Artist: ", mapArtist)
+      #I dont see a point in grabbing more than 25
+      mapID = api.get_user_best(userid, limit=25)[i].beatmap_id
+      map = api.get_beatmaps(beatmap_id=mapID)[0]
+      mapTitle = map.title
+      mapArtist = map.artist
+      print(mapID)
+      print("Title:", mapTitle, ", Artist:", mapArtist)
 
 
 def main():
    pullTopPlays("9042348")
    
+if __name__ == "__main__":
+    main()
 
 
 
